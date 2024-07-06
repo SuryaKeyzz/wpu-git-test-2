dfcb6126e034e86f19d116ff0dbcddf092e47f82
SuryaKeyzz <gilbertsuryali1@gmail.com>
2024-07-06 22:31:09 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello WOrld</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, quidem voluptate reiciendis illo architecto assumenda. Eligendi ipsam blanditiis quas dolorem repellat laudantium error, veritatis amet cumque modi ab iusto vitae.</p>
+    <ul>
+        <li>sandhika galih</li>
+    </ul>
+
 </body>
 </html>
